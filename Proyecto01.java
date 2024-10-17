1b49bf5768ffc4575beec0ad9db58e723baafd6a
MatthewFlores2006 <cuentaalternativamatthewflores@gmail.com>
2024-10-17 12:34:38 -0500

    primer commit

---
+++ Proyecto01.java
@@ -0,0 +1,8 @@
+public class Proyecto01 {
+
+    public static void main(String[] args) {
+        System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+    }
+    
+}
+ 
